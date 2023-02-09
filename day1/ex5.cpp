3ad8c3353ebc0ac747b337e1cc3812bb044890f1
ming9oo <mj10050203@gmail.com>
2023-02-09 10:17:48 +0900

    이름공간에 따른 함수 호출

---
+++ day1/ex5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+namespace BestComlmpl {
+	void SimpleFunc(void) {
+		std::cout << "BestCom이 정의한 함수" << std::endl;
+	}
+}
+
+namespace ProgComlmpl {
+	void SimpleFunc(void) {
+		std::cout << "ProgCom이 정의한 함수" << std::endl;
+	}
+}
+
+int main(void) {
+	BestComlmpl::SimpleFunc();
+	ProgComlmpl::SimpleFunc();
+	return 0;
+}
